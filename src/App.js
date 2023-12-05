2b5b3e8644f11b104e3649c91429562266cedf76
OZDOKUR <ozdokur_onur@hotmail.com>
2023-12-05 13:58:24 +0300

    "main commit"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  function addItem() {
+    if (!newItem) {
+      alert("Bir öğe girin");
+      return;
+    }
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem,
+    };
+    setItems((oldItems) => [...oldItems, item]);
+    setNewItem("");
+  }
+
+  function deleteItem(id) {
+    const newArray = items.filter((item) => item.id !== id);
+    setItems(newArray);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      <h1>Yapılacaklar Listesi</h1>
+      <div>
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Bir öğe ekleyin"
+            aria-label="Bir öğe ekleyin"
+            aria-describedby="button-addon2"
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+          ></input>
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            id="button-addon2"
+            onClick={() => addItem()}
+          >
+            Ekle
+          </button>
+        </div>
+      </div>
+
+      <ul></ul>
+      <ul className="list-group list-group-flush">
+        {items.map((item) => {
+          return (
+            <li
+              className="list-group-item d-flex justify-content-between align-items-center"
+              key={item.id}
+            >
+              {item.value}
+              <button
+                className="btn btn-danger"
+                onClick={() => deleteItem(item.id)}
+              >
+                Sil
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
